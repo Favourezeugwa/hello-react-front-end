0a6533544bf52f49f53db902ee92af88164f82fd
Favourezeugwa <favourezeugwa@gmail.com>
2022-11-09 21:21:44 +0100

    Remove extra trailing space

--- src/redux/greeting/greetingSlice.js
+++ src/redux/greeting/greetingSlice.js
@@ -1,18 +1,18 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const baseUrl = "http://localhost:3001/api/v1/messages";
+const baseUrl = 'http://localhost:3001/api/v1/messages';
 
 export const fetchMessage = createAsyncThunk(
-  "message/fetchMessage",
+  'message/fetchMessage',
   async () => {
     const response = await fetch(baseUrl);
     const data = await response.json();
     return data;
-  }
+  },
 );
 
 const greetingSlice = createSlice({
-  name: "message",
+  name: 'message',
   initialState: [],
   extraReducers: {
     [fetchMessage.fulfilled]: (state, action) => {
